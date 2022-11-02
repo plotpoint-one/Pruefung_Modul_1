ffcd8811ad786d979e70409a750958289f7c1575
Codecool-Rainer <104130086+Codecool-Rainer@users.noreply.github.com>
2022-11-02 08:30:01 +0100

    Bild2 mit Eventlistener

--- Zusammenfassungen/image_handling/main.js
+++ Zusammenfassungen/image_handling/main.js
@@ -6,5 +6,12 @@ const newImage2 = new Image();
 newImage2.src = "./images/PIC_20220718_082807.jpeg";
 newImage2.style.width = "250px";
 
+newImage2.addEventListener("click", function () {
+  console.log("newImage2 consoleclicked");
+  alert("newImage2 writeclicked");
+});
+
 document.body.appendChild(newImage1);
 document.body.appendChild(newImage2);
+
+

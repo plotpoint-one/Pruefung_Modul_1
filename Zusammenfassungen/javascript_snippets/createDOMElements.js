f2d7056fc4c92b89ff27f4e535271529297ec2c6
Codecool-Rainer <104130086+Codecool-Rainer@users.noreply.github.com>
2022-11-06 08:39:51 +0100

    Start Lessons

--- Zusammenfassungen/javascript_snippets/createDOMElements.js
+++ Zusammenfassungen/javascript_snippets/createDOMElements.js
@@ -5,7 +5,6 @@ console.log("createDOMElements.js is online");
 ///CREATING ELEMENTS AND NODES
 /////////////////////////////////////////
 
-
 ////////Create a "p"-Element///////////////////////////////////////
 const newParagraph = document.createElement("p");
 //Create a "TextNode"
@@ -17,7 +16,6 @@ newParagraph.appendChild(newTextNode);
 //Append "p" to "body"
 document.body.appendChild(newParagraph);
 
-
 ////////Create a "h1"-Element///////////////////////////////////////
 const newHeaderH1 = document.createElement("h1");
 //Create a "newHeaderText" for the "h1"
@@ -28,7 +26,6 @@ const newHeaderText = document.createTextNode(
 newHeaderH1.appendChild(newHeaderText);
 //Insert "newHeader1" before "newParagraph"
 newParagraph.parentNode.insertBefore(newHeaderH1, newParagraph);
-
 
 ////////////////Create a "br"-Carrige-Return-Element////////////////
 const carrigeReturn = document.createElement("br");
@@ -42,17 +39,15 @@ const cloneCarrigeReturn1 = carrigeReturn.cloneNode();
 //ALTERNATIVE!!!/////Insert as a sibling of carrigeReturn1///
 //carrigeReturn1.parentNode.insertBefore(cloneCarrigeReturn1, carrigeReturn1);
 
-
 ////////////////Create an "input"-Element////////////////////////////
 const newInputBox = document.createElement("input");
 //Give "newInputBox" a placeholder Text
 newInputBox.placeholder = "newInputBox.placeholder";
 //Append "newInputBox" to "neParagraph"
 newParagraph.parentNode.appendChild(newInputBox);
-//Give "newInputBox" an id for the "label"-Field. 
+//Give "newInputBox" an id for the "label"-Field.
 //In the moment only necessary, if newLabel will be dedicated
 //newInputBox.id = "newInputBox";
-
 
 //////////////Create a "button"-Element///////////////////////////////
 const newButton = document.createElement("button");
@@ -64,7 +59,6 @@ newInputBox.parentNode.appendChild(newButton);
 //Difference because of "br" in TextNode
 ///document.body.appendChild(newButton);
 
-
 //////////////Create a "label"-Field for the "newInputBox////////////
 const newLabel = document.createElement("label");
 //Text on Label
@@ -74,26 +68,24 @@ newLabel.innerText = "newLabel for newInputBox:";
 //Attach it to "newInputBox"
 newInputBox.parentNode.insertBefore(newLabel, newInputBox);
 
-
 ///////Create a "dropdown"-Menue//////////////////////////
 ///////First Attempt//////////////////////////////////////
 const dropDown = document.createElement("select");
 ////Create the "options" in "dropDown"
-let sumOfOptions =5;
-  for (i=0;i<=sumOfOptions;i++){
-    const option = document.createElement("option");
-    option.innerText = "option"+i;
-    dropDown.appendChild(option)
-  }
+let sumOfOptions = 5;
+for (i = 0; i <= sumOfOptions; i++) {
+  const option = document.createElement("option");
+  option.innerText = "option" + i;
+  dropDown.appendChild(option);
+}
 
-  //Cosmetical two times "br"
-  const cloneCarrigeReturn2 = carrigeReturn.cloneNode();
-  const cloneCarrigeReturn3 = carrigeReturn.cloneNode();
-  newInputBox.parentNode.appendChild(cloneCarrigeReturn2);
-  newInputBox.parentNode.appendChild(cloneCarrigeReturn3);
-  //Appand "dropDown" to "inputBox"
-  newInputBox.parentNode.appendChild(dropDown);
-
+//Cosmetical two times "br"
+const cloneCarrigeReturn2 = carrigeReturn.cloneNode();
+const cloneCarrigeReturn3 = carrigeReturn.cloneNode();
+newInputBox.parentNode.appendChild(cloneCarrigeReturn2);
+newInputBox.parentNode.appendChild(cloneCarrigeReturn3);
+//Appand "dropDown" to "inputBox"
+newInputBox.parentNode.appendChild(dropDown);
 
 ///////Create a "dropdown"-Menue//////////////////////////
 ///////Alternative Attempt//////////////////////////////////////
@@ -102,42 +94,63 @@ const cloneCarrigeReturn4 = carrigeReturn.cloneNode();
 const cloneCarrigeReturn5 = carrigeReturn.cloneNode();
 //body.appendChild(cloneCarrigeReturn4);
 
-
-
-////////////Create Minimal-"table"///////////
+////////////Create a Minimal-"table"///////////
 const table = document.createElement("table");
 //Create 5 table-rows "tr" (Zeilen)
-for (j=0;j<=5;j++){
-const row = document.createElement("tr");
-//Create 5 table-columns "td" (Spalte) inside the "tr"
-for (i=0;i<=5;i++){
-const cell = document.createElement("td");
-cell.innerText=`column ${i}, row ${j}`;
-row.appendChild(cell);
+for (j = 0; j <= 5; j++) {
+  const row = document.createElement("tr");
+  //Create 5 table-columns "td" (Spalte) inside the "tr"
+  for (i = 0; i <= 5; i++) {
+    const cell = document.createElement("td");
+    cell.innerText = `column ${i}, row ${j}`;
+    row.appendChild(cell);
+  }
+  table.appendChild(row);
 }
-table.appendChild(row);
-}
-
-
-
-
-
-table.setAttribute("border", "1")
-
-
+//table Cosmetics
+table.setAttribute("border", "1");
+//Attach table to "body"
 document.body.appendChild(table);
 //Cosmetics
 table.parentNode.insertBefore(cloneCarrigeReturn4, table);
 table.parentNode.insertBefore(cloneCarrigeReturn5, table);
 
-////////////Create "list"
-
-
-
 //////////////Create "div"
 
+//////////////Create "img"//////////////////////
+const image = document.createElement("img");
+image.src = "./images/PIC_20220717_055336.jpeg";
+//image.width = 600; //Achtung!! Keine px, wenn ohne style!!!
+image.style.width = "400px"; 
+//Appand "image"
+document.body.appendChild(image);
+
+////////////Cosmetic "br"- CarrigeReturn Clone without "const" BEFORE "image"
+image.parentNode.insertBefore(carrigeReturn.cloneNode(), image);
+
+////////////Create li "list"
+const unsortedList = document.createElement("ul");
+unsortedList.innerText= "unsortedList-Header"
+const list = document.createElement("li");
+for (i=0;i<=5;i++){
+  const list = document.createElement("li");
+  list.innerText = `item ${i}`
+  unsortedList.appendChild(list);
+}
+document.body.appendChild(unsortedList);
 
 
-//////////////Create "img"
+//////////////Create 5items "option"-Field Radiobutton
+for (i=0;i<=5;i++){
+const radioButton = document.createElement("input");
+radioButton.type="radio";
+const label = document.createElement("label")
+label.innerText = "radio"+i;
+const newLine = document.createElement("br");
+
+document.body.appendChild(radioButton);
+document.body.appendChild(label);
+document.body.appendChild(newLine);
+}
 
 
